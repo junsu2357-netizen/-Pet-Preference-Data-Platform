bbc8993038f26acd4f2a53b19292d4cdf4d45fb7
suhyun9024-ux <suhyun9024@gmail.com>
2026-05-09 17:18:59 +0900

    Add service worker for caching and offline support

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "danq-pwa-v1";
+  const FILES_TO_CACHE = [
+    "./",
+    "./danq_work.html",
+    "./manifest.json",
+    "./assetspaid-report-center.png"
+  ];
+
+  self.addEventListener("install", event => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(cache =>
+cache.addAll(FILES_TO_CACHE))
+    );
+    self.skipWaiting();
+  });
+
+  self.addEventListener("activate", event => {
+    event.waitUntil(
+      caches.keys().then(keys =>
+        Promise.all(keys.map(key => key !== CACHE_NAME ?
+caches.delete(key) : null))
+      )
+    );
+    self.clients.claim();
+  });
+
+  self.addEventListener("fetch", event => {
+    event.respondWith(
+      caches.match(event.request).then(response => response ||
+fetch(event.request))
+    );
+  });
